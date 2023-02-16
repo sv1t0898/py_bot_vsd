ea56f05b9b902b233330238756271a22b4c1385c
Савушкин Виктор Игоревич <savushkinvi@tgvk.local>
2023-02-16 14:48:39 +0300

    create bot py

---
+++ test_bot_py.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("5893227796:AAHj9t6W1epzcjmNCBn_4zpx0YuOIDAViLg").build()
+
+app.add_handler(CommandHandler("hello", hello))
+print("start_bot")
+app.run_polling()
